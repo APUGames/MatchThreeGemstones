cad47fffd593c288d4baac9e743b28822910ae39
GoldenPiggee <bbfrancisco22@gmail.com>
2023-04-10 19:21:50 -0700

    Tried to add movement stuffs

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -9,18 +9,34 @@ public class GridController : MonoBehaviour
     [SerializeField]
     private GameObject gemPurp;
     [SerializeField]
-    private GameObject gemPink;
+    private GameObject gemRed;
     [SerializeField]
     private GameObject gemGreen;
     [SerializeField]
     private GameObject gemYellow;
     [SerializeField]
     private GameObject gemOrange;
-    //instandiating for differnt positions of prefabs does not work unfortunately
 
 
     [SerializeField]
     private Vector3 originPosition;
+
+    public bool pressedDown;
+    public Vector2 pressedDownPosition;
+    public GameObject pressedDownGameObject;
+    public Vector2 pressedUpPosition;
+    public GameObject pressedUpGameObject;
+
+    private Vector2 startMovementPiecePosition;
+    private Vector2 endMovementPiecePosition;
+
+    public bool validMoveInProcess = false;
+
+    [Header("UI")]
+    [SerializeField]
+    private GameObject matchesFoundText;
+
+    private int matchesFound;
 
     // This script will manage the grid of pieces
     private Piece[,] grid = new Piece[8, 8];
@@ -34,7 +50,9 @@ public class GridController : MonoBehaviour
             {
                 Vector3 newWorldPosition = new Vector3(originPosition.x + row, originPosition.y, originPosition.z - column);
 
-                grid[row, column] = new Piece(newWorldPosition, new Vector2(row, column));
+                //grid[row, column] = new Piece(newWorldPosition, new Vector2(row, column));
+
+                Piece newPiece = new Piece(newWorldPosition, new Vector2(row, column)); 
 
                 System.Random rand = new System.Random();
 
@@ -51,7 +69,7 @@ public class GridController : MonoBehaviour
                 }
                 else if (randomNum >= 30 && randomNum < 45)
                 {
-                    Instantiate(gemPink, grid[row, column].GetPosition(), Quaternion.identity);
+                    Instantiate(gemRed, grid[row, column].GetPosition(), Quaternion.identity);
 
                 }
                 else if (randomNum >= 45 && randomNum < 60)
@@ -70,6 +88,7 @@ public class GridController : MonoBehaviour
 
                 }
 
+                grid[row, column] = newPiece;
 
             }
 
@@ -77,9 +96,72 @@ public class GridController : MonoBehaviour
 
     }
 
+
     // Update is called once per frame
     void Update()
     {
+        if (validMoveInProcess)
+        {
+            Vector3 placeHolderPosition = pressedDownGameObject.transform.position;
+            pressedDownGameObject.transform.position = pressedUpGameObject.transform.position;
+            pressedUpGameObject.transform.position = placeHolderPosition;
 
+
+            Piece placeHolderPiece = grid[(int)endMovementPiecePosition.x, (int)endMovementPiecePosition.y];
+            grid[(int)endMovementPiecePosition.x, (int)endMovementPiecePosition.y] = grid[(int)startMovementPiecePosition.x, (int)startMovementPiecePosition.y];
+            grid[(int)startMovementPiecePosition.x, (int)startMovementPiecePosition.y] = placeHolderPiece;
+
+            validMoveInProcess = false;
+
+            matchesFound += 1;
+        }
+    }
+
+    public void ValidMove(Vector2 start, Vector2 end)
+    {
+        Debug.Log("validating start (" + start.x + ", " + start.y + ") | end (" + end.x + ", " + end.y + ")");
+        startMovementPiecePosition = start;
+        endMovementPiecePosition = end;
+
+        bool matchFound = false;
+
+        if (!matchFound)
+        {
+            try
+            {
+                Piece topPiece1 = grid[(int)end.x, (int)end.y - 1];
+                Piece bottomPiece1 = grid[(int)end.x, (int)end.y + 1];
+                Debug.Log("Top piece type: " + topPiece1.GetPieceType());
+                Debug.Log("Bottom piece type: " + bottomPiece1.GetPieceType());
+                Piece midPiece1 = grid[(int)start.x, (int)start.y];
+                Piece toDestroy1 = grid[(int)end.x, (int)end.y];
+                Debug.Log("Mid piece type: " + midPiece1.GetPieceType());
+                if (topPiece1.GetPieceType() == bottomPiece1.GetPieceType())
+                {
+                    if (topPiece1.GetPieceType() == midPiece1.GetPieceType())
+                    {
+                        matchFound = true;
+                        validMoveInProcess = true;
+                        topPiece1.SetForDestruction();
+                        bottomPiece1.SetForDestruction();
+                        toDestroy1.SetForDestruction();
+                        Debug.Log("It Work");
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+
+            }
+        }
+
+        Debug.Log("No move fore you");
+    }
+
+    public bool IsDestroyed(Vector2 gridPosition)
+    {
+        Piece piece = grid[(int)gridPosition.x, (int)gridPosition.y];
+        return piece.GetDestruction();
     }
 }
+
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,16 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PieceTypes
+{
+    GemBlue = 1,
+    GemPurple = 2,
+    GemRed = 3,
+    GemGreen = 4,
+    GemYellow = 5,
+    GemOrange = 6
+}
 public class Piece
 {
     private Vector3 position;
     private Vector2 gridPosition;
-
+    private PieceTypes pieceType;
+    private bool setForDestruction;
 
     public Piece()
     {
         position = Vector3.zero;
         gridPosition = Vector2.zero;
+        setForDestruction = false;
 
     }
 
@@ -19,10 +30,43 @@ public class Piece
     {
         this.position = position;
         this.gridPosition = gridPosition;
+        this.setForDestruction = false;
+    }
+    public Piece(Vector3 position, Vector2 gridPosition, PieceTypes pieceType)
+    {
+        this.position = position;
+        this.gridPosition = gridPosition;
+        this.pieceType = pieceType;
+        this.setForDestruction = false;
+    }
+
+    public void SetForDestruction()
+    {
+        this.setForDestruction = true;
+    }
+
+    public void SetGridPosition(Vector2 position)
+    {
+        this.gridPosition = position;
     }
 
     public Vector3 GetPosition()
     {
         return position;
+    }
+
+    public Vector3 GetGridPosition()
+    {
+        return gridPosition;
+    }
+
+    public PieceTypes GetPieceType()
+    {
+        return pieceType;
+    }
+
+    public bool GetDestruction()
+    {
+        return setForDestruction;
     }
 }
